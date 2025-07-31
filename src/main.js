993136aff4e59c2d28504d289084cce989edcb70
Anurag Kumawat <anurag.kmwt7851@gmail.com>
2025-07-31 14:16:52 +0530

    ok

--- src/main.js
+++ src/main.js
@@ -44,7 +44,7 @@ export default async (context) => {
 
       if (!userId) {
         error('User ID not found in request.');
-        return res.redirect(failureUrl || "https://localhost:5173/login", 303);
+        return res.json({url:"localhost:5173/login"}, 302);
       }
 
       const session = await stripe.checkoutPayment(
